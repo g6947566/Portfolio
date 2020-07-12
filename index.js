33c7bc57e2b10df8c4728ac11ca8e8e1c92352f3
NL <NL@NLs-MacBook-Air.fios-router.home>
2020-07-12 12:09:43 -0400

    copy from branch to master

--- index.js
+++ index.js
@@ -5,21 +5,36 @@ function handleHamburgerClick(){
     $(".toggle").on("click", function(){
         if ($(".item").hasClass("active")){
             $(".item").removeClass("active");
+            //setting to $(this) is saying go to .item at "a" and add the "hamburger" icon.
             $(this).find("a").html("<i class='fas fa-bars'></i>");
         }else {
             $(".item").addClass("active");
+            //setting to $(this) is saying go to .item at "a" and add the "x" icon.
             $(this).find("a").html("<i class='fas fa-times'></i>");
         
         }
     });
 };
 
-function buttonClick(){
-    
-}
 
+//click on the body will start the closeMenu Function
+$("body").click( function(e) {
+    closeMenu();
+    //by setting at .toggle instead of this it won't change all links in nav to Hamburger icon.
+    $(".toggle").find("a").html("<i class='fas fa-bars'></i>")
+});
 
-
+//close function for hamburger
+function closeMenu(){
+    /*This remove the hamburger bar after body is clicked
+    $('.toggle').fadeOut(200);*/
+    $('.item').removeClass('active');  
+  }
+ 
+  
+  $(".toggle").click( function(e) {
+      e.stopPropagation(); // this stops the event from bubbling up to the body
+  });
 
 
 
@@ -27,6 +42,9 @@ function buttonClick(){
 function appList(){
     handleHamburgerClick();
     console.log('appList works');
+    gsap.to(".box", {rotation: 1080, x: 120, duration: 6});
+
+
 }
 
 $(appList);
